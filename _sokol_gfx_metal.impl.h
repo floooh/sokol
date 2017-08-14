ebaf5b9d67fd265ed978e1a1ad299e446a2f6ebd
Andre Weissflog <floooh@gmail.com>
2017-08-14 20:47:16 +0200

    Metal: enough code to render triangle

--- _sokol_gfx_metal.impl.h
+++ _sokol_gfx_metal.impl.h
@@ -91,6 +91,17 @@ _SOKOL_PRIVATE MTLPrimitiveTopologyClass _sg_mtl_primitive_topology_class(sg_pri
     }
 }
 
+_SOKOL_PRIVATE MTLPrimitiveType _sg_mtl_primitive_type(sg_primitive_type t) {
+    switch (t) {
+        case SG_PRIMITIVETYPE_POINTS:           return MTLPrimitiveTypePoint;
+        case SG_PRIMITIVETYPE_LINES:            return MTLPrimitiveTypeLine;
+        case SG_PRIMITIVETYPE_LINE_STRIP:       return MTLPrimitiveTypeLineStrip;
+        case SG_PRIMITIVETYPE_TRIANGLES:        return MTLPrimitiveTypeTriangle;
+        case SG_PRIMITIVETYPE_TRIANGLE_STRIP:   return MTLPrimitiveTypeTriangleStrip;
+        default: SOKOL_UNREACHABLE; break;
+    }
+}
+
 _SOKOL_PRIVATE MTLPixelFormat _sg_mtl_rendertarget_color_format(sg_pixel_format fmt) {
     switch (fmt) {
         case SG_PIXELFORMAT_RGBA8:          return MTLPixelFormatBGRA8Unorm;    /* not a bug */
@@ -165,7 +176,7 @@ _SOKOL_PRIVATE MTLBlendFactor _sg_mtl_blend_factor(sg_blend_factor f) {
         case SG_BLENDFACTOR_ONE_MINUS_BLEND_COLOR:  return MTLBlendFactorOneMinusBlendColor;
         case SG_BLENDFACTOR_BLEND_ALPHA:            return MTLBlendFactorBlendAlpha;
         case SG_BLENDFACTOR_ONE_MINUS_BLEND_ALPHA:  return MTLBlendFactorOneMinusBlendAlpha;
-        default: SOKOL_UNREACHABLE; break;
+        default: SOKOL_UNREACHABLE; return 0;
     }
 }
 
@@ -177,9 +188,9 @@ _SOKOL_PRIVATE MTLCompareFunction _sg_mtl_compare_func(sg_compare_func f) {
         case SG_COMPAREFUNC_LESS_EQUAL:     return MTLCompareFunctionLessEqual;
         case SG_COMPAREFUNC_GREATER:        return MTLCompareFunctionGreater;
         case SG_COMPAREFUNC_NOT_EQUAL:      return MTLCompareFunctionNotEqual;
-        case SG_COMPAREFUNC_GREATER_EQUAL:  return MTLCompareFunctionGreater;
+        case SG_COMPAREFUNC_GREATER_EQUAL:  return MTLCompareFunctionGreaterEqual;
         case SG_COMPAREFUNC_ALWAYS:         return MTLCompareFunctionAlways;
-        default: SOKOL_UNREACHABLE; break;
+        default: SOKOL_UNREACHABLE; return 0;
     }
 }
 
@@ -193,7 +204,32 @@ _SOKOL_PRIVATE MTLStencilOperation _sg_mtl_stencil_op(sg_stencil_op op) {
         case SG_STENCILOP_INVERT:       return MTLStencilOperationInvert;
         case SG_STENCILOP_INCR_WRAP:    return MTLStencilOperationIncrementWrap;
         case SG_STENCILOP_DECR_WRAP:    return MTLStencilOperationDecrementWrap;
-        default: SOKOL_UNREACHABLE; break;
+        default: SOKOL_UNREACHABLE; return 0;
+    }
+}
+
+_SOKOL_PRIVATE MTLCullMode _sg_mtl_cull_mode(sg_cull_mode m) {
+    switch (m) {
+        case SG_CULLMODE_NONE:  return MTLCullModeNone;
+        case SG_CULLMODE_FRONT: return MTLCullModeFront;
+        case SG_CULLMODE_BACK:  return MTLCullModeBack;
+        default: SOKOL_UNREACHABLE; return 0;
+    }
+}
+
+_SOKOL_PRIVATE MTLWinding _sg_mtl_winding(sg_face_winding w) {
+    switch (w) {
+        case SG_FACEWINDING_CW:     return MTLWindingClockwise;
+        case SG_FACEWINDING_CCW:    return MTLWindingCounterClockwise;
+        default: SOKOL_UNREACHABLE; return 0;
+    }
+}
+
+_SOKOL_PRIVATE MTLIndexType _sg_mtl_index_type(sg_index_type t) {
+    switch (t) {
+        case SG_INDEXTYPE_UINT16:   return MTLIndexTypeUInt16;
+        case SG_INDEXTYPE_UINT32:   return MTLIndexTypeUInt32;
+        default: SOKOL_UNREACHABLE; return 0;
     }
 }
 
@@ -366,8 +402,13 @@ typedef struct {
     _sg_slot slot;
     _sg_shader* shader;
     sg_shader shader_id;
-    sg_primitive_type primitive_type;
     sg_index_type index_type;
+    MTLPrimitiveType mtl_prim_type;
+    MTLIndexType mtl_index_type;
+    MTLCullMode mtl_cull_mode;
+    MTLWinding mtl_winding;
+    float blend_color[4];
+    uint32_t mtl_stencil_ref;
     uint32_t mtl_rps;
     uint32_t mtl_dss;
 } _sg_pipeline;
@@ -399,6 +440,12 @@ _SOKOL_PRIVATE void _sg_init_pass(_sg_pass* pass) {
 static bool _sg_mtl_valid;
 static bool _sg_mtl_in_pass;
 static bool _sg_mtl_pass_valid;
+static int _sg_mtl_cur_width;
+static int _sg_mtl_cur_height;
+static _sg_pipeline* _sg_mtl_cur_pipeline;
+static sg_pipeline _sg_mtl_cur_pipeline_id;
+static _sg_buffer* _sg_mtl_cur_indexbuffer;
+static sg_buffer _sg_mtl_cur_indexbuffer_id;
 static const void*(*_sg_mtl_renderpass_descriptor_cb)(void);
 static const void*(*_sg_mtl_drawable_cb)(void);
 static uint32_t _sg_mtl_frame_index;
@@ -422,6 +469,10 @@ _SOKOL_PRIVATE void _sg_setup_backend(const sg_desc* desc) {
     _sg_mtl_valid = true;
     _sg_mtl_in_pass = false;
     _sg_mtl_pass_valid = false;
+    _sg_mtl_cur_width = 0;
+    _sg_mtl_cur_height = 0;
+    _sg_mtl_cur_pipeline = 0;
+    _sg_mtl_cur_pipeline_id.id = SG_INVALID_ID;
     _sg_mtl_renderpass_descriptor_cb = desc->mtl_renderpass_descriptor_cb;
     _sg_mtl_drawable_cb = desc->mtl_drawable_cb;
     _sg_mtl_frame_index = 1;
@@ -537,7 +588,7 @@ _SOKOL_PRIVATE id<MTLLibrary> _sg_mtl_compile_library(const char* src) {
 _SOKOL_PRIVATE void _sg_create_shader(_sg_shader* shd, const sg_shader_desc* desc) {
     SOKOL_ASSERT(shd && desc);
     SOKOL_ASSERT(shd->slot.state == SG_RESOURCESTATE_ALLOC);
-    SOKOL_ASSERT(desc->vs.func && desc->fs.func);
+    SOKOL_ASSERT(desc->vs.entry && desc->fs.entry);
     /* vertex-shader as MTLLibrary */
     id<MTLLibrary> vs_lib;
     if (desc->vs.source) {
@@ -562,8 +613,8 @@ _SOKOL_PRIVATE void _sg_create_shader(_sg_shader* shd, const sg_shader_desc* des
         shd->slot.state = SG_RESOURCESTATE_FAILED;
         return;
     }
-    id<MTLFunction> vs_func = [vs_lib newFunctionWithName:[NSString stringWithUTF8String:desc->vs.func]];
-    id<MTLFunction> fs_func = [fs_lib newFunctionWithName:[NSString stringWithUTF8String:desc->fs.func]];
+    id<MTLFunction> vs_func = [vs_lib newFunctionWithName:[NSString stringWithUTF8String:desc->vs.entry]];
+    id<MTLFunction> fs_func = [fs_lib newFunctionWithName:[NSString stringWithUTF8String:desc->fs.entry]];
     if (nil == vs_func) {
         SOKOL_LOG("vertex shader function not found\n");
         shd->slot.state = SG_RESOURCESTATE_FAILED;
@@ -600,8 +651,18 @@ _SOKOL_PRIVATE void _sg_create_pipeline(_sg_pipeline* pip, _sg_shader* shd, cons
 
     pip->shader = shd;
     pip->shader_id = desc->shader;
-    pip->primitive_type = _sg_select(desc->primitive_type, SG_PRIMITIVETYPE_TRIANGLES);
     pip->index_type = _sg_select(desc->index_type, SG_INDEXTYPE_NONE);
+    sg_primitive_type prim_type = _sg_select(desc->primitive_type, SG_PRIMITIVETYPE_TRIANGLES);
+    pip->mtl_prim_type = _sg_mtl_primitive_type(prim_type);
+    pip->mtl_cull_mode = _sg_mtl_cull_mode(_sg_select(desc->rasterizer.cull_mode, SG_CULLMODE_NONE));
+    pip->mtl_winding = _sg_mtl_winding(_sg_select(desc->rasterizer.face_winding, SG_FACEWINDING_CW));
+    pip->mtl_stencil_ref = desc->depth_stencil.stencil_ref;
+    if (SG_INDEXTYPE_NONE != pip->index_type) {
+        pip->mtl_index_type = _sg_mtl_index_type(pip->index_type);
+    }
+    for (int i = 0; i < 4; i++) {
+        pip->blend_color[i] = desc->blend.blend_color[i];
+    }
 
     /* create vertex-descriptor */
     MTLVertexDescriptor* vtx_desc = [MTLVertexDescriptor vertexDescriptor];
@@ -611,8 +672,8 @@ _SOKOL_PRIVATE void _sg_create_pipeline(_sg_pipeline* pip, _sg_shader* shd, cons
             break;
         }
         vtx_desc.layouts[layout_index].stride = layout_desc->stride;
-        vtx_desc.layouts[layout_index].stepFunction = _sg_mtl_step_function(layout_desc->step_func);
-        vtx_desc.layouts[layout_index].stepRate = layout_desc->step_rate;
+        vtx_desc.layouts[layout_index].stepFunction = _sg_mtl_step_function(_sg_select(layout_desc->step_func, SG_VERTEXSTEP_PER_VERTEX));
+        vtx_desc.layouts[layout_index].stepRate = _sg_select(layout_desc->step_rate, 1);
         for (int attr_index = 0; attr_index < SG_MAX_VERTEX_ATTRIBUTES; attr_index++) {
             const sg_vertex_attr_desc* attr_desc = &layout_desc->attrs[attr_index];
             if (attr_desc->format == SG_VERTEXFORMAT_INVALID) {
@@ -633,7 +694,7 @@ _SOKOL_PRIVATE void _sg_create_pipeline(_sg_pipeline* pip, _sg_shader* shd, cons
     rp_desc.alphaToCoverageEnabled = desc->rasterizer.alpha_to_coverage_enabled;
     rp_desc.alphaToOneEnabled = NO;
     rp_desc.rasterizationEnabled = YES;
-    rp_desc.inputPrimitiveTopology = _sg_mtl_primitive_topology_class(pip->primitive_type);
+    rp_desc.inputPrimitiveTopology = _sg_mtl_primitive_topology_class(prim_type);
     rp_desc.depthAttachmentPixelFormat = _sg_mtl_rendertarget_depth_format(_sg_select(desc->blend.depth_format, SG_PIXELFORMAT_DEPTHSTENCIL));
     rp_desc.stencilAttachmentPixelFormat = _sg_mtl_rendertarget_stencil_format(_sg_select(desc->blend.depth_format, SG_PIXELFORMAT_DEPTHSTENCIL));
     const int mrt_count = _sg_select(desc->blend.mrt_count, 1);
@@ -713,6 +774,8 @@ _SOKOL_PRIVATE void _sg_begin_pass(_sg_pass* pass, const sg_pass_action* action,
     SOKOL_ASSERT(!_sg_mtl_cmd_encoder);
     SOKOL_ASSERT(_sg_mtl_renderpass_descriptor_cb);
     _sg_mtl_in_pass = true;
+    _sg_mtl_cur_width = w;
+    _sg_mtl_cur_height = h;
 
     /* if this is the first pass in the frame, create a command buffer */
     if (nil == _sg_mtl_cmd_buffer) {
@@ -823,11 +886,48 @@ _SOKOL_PRIVATE void _sg_commit() {
 }
 
 _SOKOL_PRIVATE void _sg_apply_viewport(int x, int y, int w, int h, bool origin_top_left) {
-    // FIXME
+    SOKOL_ASSERT(_sg_mtl_in_pass);
+    if (!_sg_mtl_pass_valid) {
+        return;
+    }
+    SOKOL_ASSERT(_sg_mtl_cmd_encoder);
+    MTLViewport vp;
+    vp.originX = (double) x;
+    vp.originY = (double) (origin_top_left ? y : (_sg_mtl_cur_height - (y + h)));
+    vp.width   = (double) w;
+    vp.height  = (double) h;
+    vp.znear   = 0.0;
+    vp.zfar    = 1.0;
+    [_sg_mtl_cmd_encoder setViewport:vp];
 }
 
+#define _sg_mtl_min(a,b) ((a<b)?a:b)
+#define _sg_mtl_max(a,b) ((a>b)?a:b)
+
 _SOKOL_PRIVATE void _sg_apply_scissor_rect(int x, int y, int w, int h, bool origin_top_left) {
-    // FIXME
+    SOKOL_ASSERT(_sg_mtl_in_pass);
+    if (!_sg_mtl_pass_valid) {
+        return;
+    }
+    SOKOL_ASSERT(_sg_mtl_cmd_encoder);
+    /* clip against framebuffer rect */
+    x = _sg_mtl_min(_sg_mtl_max(0, x), _sg_mtl_cur_width-1);
+    y = _sg_mtl_min(_sg_mtl_max(0, y), _sg_mtl_cur_height-1);
+    if ((x + w) > _sg_mtl_cur_width) {
+        w = _sg_mtl_cur_width - x;
+    }
+    if ((y + h) > _sg_mtl_cur_height) {
+        h = _sg_mtl_cur_height - y;
+    }
+    w = _sg_mtl_max(w, 1);
+    h = _sg_mtl_max(h, 1);
+
+    MTLScissorRect r;
+    r.x = x;
+    r.y = origin_top_left ? y : (_sg_mtl_cur_height - (y + h));
+    r.width = w;
+    r.height = h;
+    [_sg_mtl_cmd_encoder setScissorRect:r];
 }
 
 _SOKOL_PRIVATE void _sg_apply_draw_state(
@@ -837,7 +937,46 @@ _SOKOL_PRIVATE void _sg_apply_draw_state(
     _sg_image** fs_imgs, int num_fs_imgs)
 {
     SOKOL_ASSERT(pip);
-    // FIXME
+    SOKOL_ASSERT(pip->shader);
+    SOKOL_ASSERT(_sg_mtl_in_pass);
+    if (!_sg_mtl_pass_valid) {
+        return;
+    }
+    SOKOL_ASSERT(_sg_mtl_cmd_encoder);
+
+    _sg_mtl_cur_pipeline = pip;
+    _sg_mtl_cur_pipeline_id.id = pip->slot.id;
+    _sg_mtl_cur_indexbuffer = ib;
+    if (ib) {
+        SOKOL_ASSERT(pip->index_type != SG_INDEXTYPE_NONE);
+        _sg_mtl_cur_indexbuffer_id.id = ib->slot.id;
+    }
+    else {
+        SOKOL_ASSERT(pip->index_type == SG_INDEXTYPE_NONE);
+        _sg_mtl_cur_indexbuffer_id.id = SG_INVALID_ID;
+    }
+
+    /* apply state */
+    const float* c = pip->blend_color;
+    [_sg_mtl_cmd_encoder setBlendColorRed:c[0] green:c[1] blue:c[2] alpha:c[3]];
+    [_sg_mtl_cmd_encoder setCullMode:pip->mtl_cull_mode];
+    [_sg_mtl_cmd_encoder setFrontFacingWinding:pip->mtl_winding];
+    [_sg_mtl_cmd_encoder setStencilReferenceValue:pip->mtl_stencil_ref];
+    [_sg_mtl_cmd_encoder setRenderPipelineState:_sg_mtl_pool[pip->mtl_rps]];
+    [_sg_mtl_cmd_encoder setDepthStencilState:_sg_mtl_pool[pip->mtl_dss]];
+
+    /* apply vertex buffers */
+    int vb_slot;
+    for (vb_slot = 0; vb_slot < num_vbs; vb_slot++) {
+        const _sg_buffer* vb = vbs[vb_slot];
+        [_sg_mtl_cmd_encoder setVertexBuffer:_sg_mtl_pool[vb->mtl_buf[vb->active_slot]] offset:0 atIndex:vb_slot];
+    }
+    for (; vb_slot < SG_MAX_SHADERSTAGE_BUFFERS; vb_slot++) {
+        [_sg_mtl_cmd_encoder setVertexBuffer:nil offset:0 atIndex:vb_slot];
+    }
+
+    /* FIXME: apply vertex shader images */
+    /* FIXME: apply fragment shader images */
 }
 
 _SOKOL_PRIVATE void _sg_apply_uniform_block(sg_shader_stage stage_index, int ub_index, const void* data, int num_bytes) {
@@ -847,7 +986,32 @@ _SOKOL_PRIVATE void _sg_apply_uniform_block(sg_shader_stage stage_index, int ub_
 }
 
 _SOKOL_PRIVATE void _sg_draw(int base_element, int num_elements, int num_instances) {
-    // FIXME
+    SOKOL_ASSERT(_sg_mtl_in_pass);
+    if (!_sg_mtl_pass_valid) {
+        return;
+    }
+    SOKOL_ASSERT(_sg_mtl_cmd_encoder);
+    SOKOL_ASSERT(_sg_mtl_cur_pipeline && (_sg_mtl_cur_pipeline->slot.id == _sg_mtl_cur_pipeline_id.id));
+    if (SG_CULLMODE_NONE != _sg_mtl_cur_pipeline->index_type) {
+        /* indexed rendering */
+        SOKOL_ASSERT(_sg_mtl_cur_indexbuffer && (_sg_mtl_cur_indexbuffer->slot.id == _sg_mtl_cur_pipeline_id.id));
+        const _sg_buffer* ib = _sg_mtl_cur_indexbuffer;
+        const NSUInteger index_byte_size = (_sg_mtl_cur_pipeline->mtl_index_type == MTLIndexTypeUInt16) ? 2 : 4;
+        const NSUInteger index_buffer_offset = base_element * index_byte_size;
+        [_sg_mtl_cmd_encoder drawIndexedPrimitives:_sg_mtl_cur_pipeline->mtl_prim_type
+            indexCount:num_elements
+            indexType:_sg_mtl_cur_pipeline->mtl_index_type
+            indexBuffer:_sg_mtl_pool[ib->mtl_buf[ib->active_slot]]
+            indexBufferOffset:index_buffer_offset
+            instanceCount:num_instances];
+    }
+    else {
+        /* non-indexed rendering */
+        [_sg_mtl_cmd_encoder drawPrimitives:_sg_mtl_cur_pipeline->mtl_prim_type
+            vertexStart:base_element
+            vertexCount:num_elements
+            instanceCount:num_instances];
+    }
 }
 
 _SOKOL_PRIVATE void _sg_update_buffer(_sg_buffer* buf, const void* data, int data_size) {
