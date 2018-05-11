719afb57cb1e2c3be48b63ffa025b820c67da450
Andre Weissflog <floooh@gmail.com>
2018-05-11 21:51:04 +0200

    continue with sokol_app.h

--- sokol_app.h
+++ sokol_app.h
@@ -66,7 +66,7 @@ typedef struct {
     bool stencil;
     bool premultiplied_alpha;
     bool preserve_drawing_buffer;
-    const char* title;
+    const char* window_title;
 } sapp_desc;
 
 typedef enum {
@@ -116,19 +116,202 @@ typedef struct {
 
 typedef void (*sapp_event_callback)(const sapp_event*);
 
+/* user-provided functions */
+extern void sokol_enter();
+extern void sokol_frame();
+extern int sokol_exit();
+
 extern void sapp_setup(const sapp_desc* desc);
 extern void sapp_shutdown();
 extern bool sapp_isvalid();
-extern bool sapp_process_events();
-extern void sapp_present();
-extern void sapp_on_event(sapp_event_callback cb);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 /*-- IMPLEMENTATION ----------------------------------------------------------*/
 #ifdef SOKOL_IMPL
+
+#ifndef SOKOL_DEBUG
+    #ifdef _DEBUG
+        #define SOKOL_DEBUG (1)
+    #endif
+#endif
 #ifndef SOKOL_ASSERT
     #include <assert.h>
     #define SOKOL_ASSERT(c) assert(c)
 #endif
+#ifndef SOKOL_LOG
+    #ifdef SOKOL_DEBUG 
+        #include <stdio.h>
+        #define SOKOL_LOG(s) { SOKOL_ASSERT(s); puts(s); }
+    #else
+        #define SOKOL_LOG(s)
+    #endif
+#endif
+#ifndef _SOKOL_PRIVATE
+    #if defined(__GNUC__)
+        #define _SOKOL_PRIVATE __attribute__((unused)) static
+    #else
+        #define _SOKOL_PRIVATE static
+    #endif
+#endif
+
+#ifdef __cplusplus
+extern "C"
+#endif
+
+/* helper macros */
+#define _sapp_def(val, def) (((val) == 0) ? (def) : (val))
+
+enum {
+    _SAPP_MAX_TITLE_LENGTH = 128,
+};
+
+typedef struct {
+    bool valid;
+    int width;
+    int height;
+    int sample_count;
+    char window_title[_SAPP_MAX_TITLE_LENGTH];
+} _sapp_state;
+static _sapp_state _sapp;
+
+/*== MacOS ===================================================================*/
+
+#if defined(__APPLE__)
+
+#if !__has_feature(objc_arc)
+#error "sokol_app.h requires ARC (Automatic Reference Counting) on MacOS and iOS"
+#endif
+
+#include <TargetConditionals.h>
+#if !TARGET_OS_IPHONE
+#import <Cocoa/Cocoa.h>
+#import <QuartzCore/QuartzCore.h>
+
+static id _sapp_window;
+static id _sapp_win_dlg;
+static id _sapp_app_dlg;
+
+@interface _sapp_app_delegate : NSObject<NSApplicationDelegate>
+@end
+@interface _sapp_window_delegate : NSObject<NSWindowDelegate>
+@end 
+
+@implementation _sapp_app_delegate
+- (void)applicationDidFinishLaunching:(NSNotification*)aNotification {
+    const NSUInteger style =
+        NSWindowStyleMaskTitled |
+        NSWindowStyleMaskClosable |
+        NSWindowStyleMaskMiniaturizable |
+        NSWindowStyleMaskResizable;
+    _sapp_window = [[NSWindow alloc]
+        initWithContentRect:NSMakeRect(0, 0, _sapp.width, _sapp.height)
+        styleMask:style
+        backing:NSBackingStoreBuffered
+        defer:NO];
+    [_sapp_window setTitle:[NSString stringWithUTF8String:_sapp.window_title]];
+    [_sapp_window setAcceptsMouseMovedEvents:YES];
+    [_sapp_window center];
+    [_sapp_window setRestorable:YES];
+    _sapp_win_dlg = [[_sapp_window_delegate alloc] init];
+    [_sapp_window setDelegate:_sapp_win_dlg];
+
+    [_sapp_window makeKeyAndOrderFront:nil];
+}
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication*)sender {
+    return YES;
+}
+@end
+
+@implementation _sapp_window_delegate
+- (BOOL)windowShouldClose:(id)sender {
+    sokol_exit();
+    return YES;
+}
+
+- (void)windowDidResize:(NSNotification*)notification {
+    /* FIXME */
+}
+
+- (void)windowDidMove:(NSNotification*)notification {
+    /* FIXME */
+}
+
+- (void)windowDidMiniaturize:(NSNotification*)notification {
+    /* FIXME */
+}
+
+- (void)windowDidDeminiaturize:(NSNotification*)notification {
+    /* FIXME */
+}
+
+- (void)windowDidBecomeKey:(NSNotification*)notification {
+    /* FIXME */
+}
+
+- (void)windowDidResignKey:(NSNotification*)notification {
+    /* FIXME */
+}
+@end
+
+_SOKOL_PRIVATE void _sapp_setup(const sapp_desc* desc) {
+    _sapp.width = _sapp_def(desc->width, 640);
+    _sapp.height = _sapp_def(desc->height, 480);
+    _sapp.sample_count = _sapp_def(desc->sample_count, 1);
+    strncpy(_sapp.window_title, desc->window_title, sizeof(_sapp.window_title));
+    _sapp.window_title[_SAPP_MAX_TITLE_LENGTH-1] = 0;
+    [NSApplication sharedApplication];
+    [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+    _sapp_app_dlg = [[_sapp_app_delegate alloc] init];
+    [NSApp setDelegate:_sapp_app_dlg];
+    [NSApp activateIgnoringOtherApps:YES];
+}
+
+_SOKOL_PRIVATE void _sapp_run() {
+    SOKOL_ASSERT(_sapp.valid);
+    [NSApp run];
+}
+
+_SOKOL_PRIVATE void _sapp_shutdown() {
+    /* FIXME */
+}
+#endif
+
+#if TARGET_OS_IPHONE
+
+#endif
+
+/* OSX/iOS entry function */
+int main() {
+    sokol_enter();
+    _sapp_run();
+    return 0;
+}
+
+#else
+#error "sokol_app.h: Unknown OS"
+#endif
+
+/*== PUBLIC API FUNCTIONS ====================================================*/
+void sapp_setup(const sapp_desc* desc) {
+    SOKOL_ASSERT(desc);
+    memset(&_sapp, 0, sizeof(_sapp));
+    _sapp_setup(desc);
+    _sapp.valid = true;
+}
+
+bool sapp_isvalid() {
+    return _sapp.valid;
+}
+
+void sapp_shutdown() {
+    _sapp_shutdown();
+    _sapp.valid = false;
+}
+
+#undef _sapp_def
 
 #endif /* SOKOL_IMPL */
 
