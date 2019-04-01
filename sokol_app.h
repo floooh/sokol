ba6062ce530e476d99dd7b53931042c20f00bc66
Andre Weissflog <floooh@gmail.com>
2019-04-01 19:38:49 +0200

    sokol_app.h: actuall fix #138 (WM_MOUSEWHEEL vs WM_MOUSEHWHEEL)

--- sokol_app.h
+++ sokol_app.h
@@ -2566,8 +2566,8 @@ _SOKOL_PRIVATE const _sapp_gl_fbconfig* _sapp_gl_choose_fbconfig(const _sapp_gl_
 #endif
 
 /* see https://github.com/floooh/sokol/issues/138 */
-#ifndef WM_MOUSEWHEEL
-#define WM_MOUSEWHEEL (0x020A)
+#ifndef WM_MOUSEHWHEEL
+#define WM_MOUSEHWHEEL (0x020E)
 #endif
 
 #ifndef DPI_ENUMS_DECLARED
