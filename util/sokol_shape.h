b422d61bc07d00eaa73d2dff353172a1724f2713
Andre Weissflog <floooh@gmail.com>
2021-01-12 19:14:15 +0100

    sokol_shape.h: temporary SOKOL_ZIG_BINDINGS ABI workarounds

--- util/sokol_shape.h
+++ util/sokol_shape.h
@@ -421,12 +421,18 @@ typedef struct sshape_vertex_t {
 typedef struct sshape_element_range_t {
     uint32_t base_element;
     uint32_t num_elements;
+    #if defined(SOKOL_ZIG_BINDINGS)
+    uint32_t __pad[3];
+    #endif
 } sshape_element_range_t;
 
 /* number of elements and byte size of build actions */
 typedef struct sshape_sizes_item_t {
     uint32_t num;       // number of elements
     uint32_t size;      // the same as size in bytes
+    #if defined(SOKOL_ZIG_BINDINGS)
+    uint32_t __pad[3];
+    #endif
 } sshape_sizes_item_t;
 
 typedef struct sshape_sizes_t {
