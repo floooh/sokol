609900f2f0c183f9b27790ec1d2a74d3484d9d91
Garett Bass <garettbass@me.com>
2021-02-25 19:29:59 -0800

    added nim support to bindgen

--- bindgen/gen_all.py
+++ bindgen/gen_all.py
@@ -1,4 +1,4 @@
-import os, gen_zig
+import os, gen_nim, gen_zig
 
 tasks = [
     [ '../sokol_gfx.h',            'sg_',       [] ],
@@ -9,6 +9,14 @@ tasks = [
     [ '../util/sokol_debugtext.h', 'sdtx_',     ['sg_'] ],
     [ '../util/sokol_shape.h',     'sshape_',   ['sg_'] ],
 ]
+
+# Nim
+gen_nim.prepare()
+for task in tasks:
+    c_header_path = task[0]
+    main_prefix = task[1]
+    dep_prefixes = task[2]
+    gen_nim.gen(c_header_path, main_prefix, dep_prefixes)
 
 # Zig
 gen_zig.prepare()
