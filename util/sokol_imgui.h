e5a69bf3c2334045ea4ac277bd07899b9e0c8d86
Shane Liesegang <shane@techie.net>
2021-10-23 14:22:11 +0300

    don't consume keyboard events if mouse is over imgui window

--- util/sokol_imgui.h
+++ util/sokol_imgui.h
@@ -2161,7 +2161,11 @@ SOKOL_API_IMPL bool simgui_handle_event(const sapp_event* ev) {
         default:
             break;
     }
-    return io->WantCaptureKeyboard || io->WantCaptureMouse;
+
+    if (ev->type == SAPP_EVENTTYPE_KEY_DOWN || ev->type == SAPP_EVENTTYPE_KEY_UP) {
+        return io->WantCaptureKeyboard;
+    }
+    return io->WantCaptureMouse;
 }
 #endif
 
