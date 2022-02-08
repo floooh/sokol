ca04823e15e2dcc8fc9272bc56ce7bdf8d3cbab2
Andre Weissflog <floooh@gmail.com>
2022-02-08 20:07:59 +0100

    sokol_app.h: don't assert in sapp_set/get_clipboard_string()

--- sokol_app.h
+++ sokol_app.h
@@ -11342,7 +11342,6 @@ SOKOL_API_IMPL void sapp_consume_event(void) {
 
 /* NOTE: on HTML5, sapp_set_clipboard_string() must be called from within event handler! */
 SOKOL_API_IMPL void sapp_set_clipboard_string(const char* str) {
-    SOKOL_ASSERT(_sapp.clipboard.enabled);
     if (!_sapp.clipboard.enabled) {
         return;
     }
@@ -11360,7 +11359,6 @@ SOKOL_API_IMPL void sapp_set_clipboard_string(const char* str) {
 }
 
 SOKOL_API_IMPL const char* sapp_get_clipboard_string(void) {
-    SOKOL_ASSERT(_sapp.clipboard.enabled);
     if (!_sapp.clipboard.enabled) {
         return "";
     }
