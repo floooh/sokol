14f7a07124000facfc83f3c653ad90a76eee6ef3
Andre Weissflog <floooh@gmail.com>
2022-07-25 18:17:08 +0200

    gen_odin.py: don't remap sgl_rad() and sgl_deg()

--- bindgen/gen_odin.py
+++ bindgen/gen_odin.py
@@ -85,8 +85,6 @@ ignores = [
 overrides = {
     'context':                              'ctx',  # reserved keyword
     'sapp_sgcontext':                       'sapp_sgctx',
-    'sgl_deg':                              'sgl_as_degrees',
-    'sgl_rad':                              'sgl_as_radians',
     'sg_context_desc.color_format':         'int',
     'sg_context_desc.depth_format':         'int',
     'SGL_NO_ERROR':                         'SGL_ERROR_NO_ERROR',
