d3de1ff1ef17f70c44ccb8dfb7e95c582360f43a
Andre Weissflog <floooh@gmail.com>
2022-08-09 20:05:51 +0200

    remove obsolete git operations in test_uwp.cmd and test_win.cmd

--- tests/test_win.cmd
+++ tests/test_win.cmd
@@ -1,6 +1,3 @@
-if not exist ext/sokol-tools-bin/ (
-    git clone --depth 1 https://github.com/floooh/sokol-tools-bin ext/sokol-tools-bin
-)
 if not exist ext/fips-cimgui/ (
     git clone --depth 1 --recursive https://github.com/fips-libs/fips-cimgui ext/fips-cimgui
 )
