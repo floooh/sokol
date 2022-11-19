359ba635b72bfbe9f371b926d7b055554979780a
Andre Weissflog <floooh@gmail.com>
2022-11-19 21:47:08 +0100

    test_linux.sh: remove build_force_egl calls

--- tests/test_linux.sh
+++ tests/test_linux.sh
@@ -5,6 +5,6 @@ build linux_gl_debug linux_gl_debug
 build linux_gl_release linux_gl_release
 build linux_gles3_debug linux_gles3_debug
 build linux_gles3_release linux_gles3_release
-build_force_egl linux_gl_egl_debug linux_gl_egl_debug
-build_force_egl linux_gl_egl_release linux_gl_egl_release
+build linux_gl_egl_debug linux_gl_egl_debug
+build linux_gl_egl_release linux_gl_egl_release
 runtest linux_gl_debug
