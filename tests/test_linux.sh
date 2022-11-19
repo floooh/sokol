56884e76a1503bcd83177f28d70a5aeb0f8a68a3
Andre Weissflog <floooh@gmail.com>
2022-11-19 21:41:33 +0100

    fix Linux tests

--- tests/test_linux.sh
+++ tests/test_linux.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -e
-source test_common.sh
+source tests/test_common.sh
 prepare
-build linux_gl_debug SOKOL_GLCORE33 Debug
-build linux_gl_release SOKOL_GLCORE33 Release
-build linux_gles3_debug SOKOL_GLES3 Debug
-build linux_gles3_release SOKOL_GLES3 Release
-build_force_egl linux_gl_egl_debug SOKOL_GLCORE33 Debug
-build_force_egl linux_gl_egl_release SOKOL_GLCORE33 Release
+build linux_gl_debug linux_gl_debug
+build linux_gl_release linux_gl_release
+build linux_gles3_debug linux_gles3_debug
+build linux_gles3_release linux_gles3_release
+build_force_egl linux_gl_egl_debug linux_gl_egl_debug
+build_force_egl linux_gl_egl_release linux_gl_egl_release
 runtest linux_gl_debug
-
