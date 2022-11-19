4696f892b14bdd57c561a77e2c1875e7ed6e75f1
Andre Weissflog <floooh@gmail.com>
2022-11-19 22:23:23 +0100

    fix android tests

--- tests/test_common.sh
+++ tests/test_common.sh
@@ -11,8 +11,8 @@ setup_emsdk() {
 }
 
 setup_android() {
-    if [ ! -d "build/android_sdk" ] ; then
-        mkdir -p build/android_sdk && cd build/android_sdk
+    if [ ! -d "tests/build/android_sdk" ] ; then
+        mkdir -p tests/build/android_sdk && cd tests/build/android_sdk
         sdk_file="sdk-tools-linux-3859397.zip"
         wget --no-verbose https://dl.google.com/android/repository/$sdk_file
         unzip -q $sdk_file
@@ -21,17 +21,15 @@ setup_android() {
         yes | ./sdkmanager "build-tools;29.0.3" >/dev/null
         yes | ./sdkmanager "platform-tools" >/dev/null
         yes | ./sdkmanager "ndk-bundle" >/dev/null
-        cd ../../../..
+        cd ../../../../..
     fi
 }
 
 build() {
     gen_preset=$1
     build_preset=$2
-#    cd ..
     cmake --preset $gen_preset
     cmake --build --preset $build_preset
-#    cd tests
 }
 
 analyze() {
@@ -60,16 +58,6 @@ analyze_ios() {
     mode=$3
     mkdir -p build/$cfg && cd build/$cfg
     cmake -GNinja -DSOKOL_BACKEND=$backend -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_BUILD_TYPE=$mode -DUSE_ANALYZER=ON ../..
-    cmake --build .
-    cd ../..
-}
-
-build_android() {
-    cfg=$1
-    backend=$2
-    mode=$3
-    mkdir -p build/$cfg && cd build/$cfg
-    cmake -GNinja -DSOKOL_BACKEND=$backend -DANDROID_ABI=armeabi-v7a -DANDROID_PLATFORM=android-28 -DCMAKE_TOOLCHAIN_FILE=../android_sdk/ndk-bundle/build/cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=$mode ../..
     cmake --build .
     cd ../..
 }
