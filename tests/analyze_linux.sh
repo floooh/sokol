48fb60f8f76372c733a58a9e6f843d4a82902da2
Andre Weissflog <floooh@gmail.com>
2022-11-20 16:26:42 +0100

    rewrite linux static analyzer build scripts to use cmake presets

--- tests/analyze_linux.sh
+++ tests/analyze_linux.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 source test_common.sh
-prepare
 
-analyze linux_gl_analyze SOKOL_GLCORE33 Debug
-analyze linux_gles3_analyze SOKOL_GLES3 Debug
+build linux_gl_analyze linux_gl_analyze
+build linux_gles3_analyze linux_gles3_analyze
