50a9bc6601739f963ad894c8951c2e2a3123ecad
Andre Weissflog <floooh@gmail.com>
2022-11-20 12:57:00 +0100

    cmake presets support for UWP tests

--- tests/test_uwp.cmd
+++ tests/test_uwp.cmd
@@ -1,18 +1,3 @@
-if not exist ext/fips-cimgui/ (
-    git clone --depth 1 --recursive https://github.com/fips-libs/fips-cimgui ext/fips-cimgui
-)
-if not exist ext/spine-runtimes/ (
-    git clone --depth 1 --recursive https://github.com/EsotericSoftware/spine-runtimes/ ext/spine-runtimes
-)
-
-md build\win_uwp_debug
-cd build\win_uwp_debug
-cmake -DSOKOL_BACKEND=SOKOL_D3D11 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0.19041.0 ../.. || exit /b 10
-cmake --build . || exit /b 10
-cd ..\..
-
-md build\win_uwp_release
-cd build\win_uwp_release
-cmake -DSOKOL_BACKEND=SOKOL_D3D11 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0.19041.0 ../.. || exit /b 10
-cmake --build . --config Release || exit /b 10
-cd ..\..
+cmake --preset win_uwp || exit /b 10
+cmake --build --preset win_uwp_debug || exit /b 10
+cmake --build --preset win_uwp_release || exit /b 10
