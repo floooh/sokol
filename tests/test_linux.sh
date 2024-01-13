5148103cc8df2a098dddd023503d464eaa065d33
Mitya Selivanov <automainint@guattari.tech>
2024-01-13 01:37:46 +0100

    GLES X11 support tests

--- tests/test_linux.sh
+++ tests/test_linux.sh
@@ -7,4 +7,6 @@ build linux_gles3_debug linux_gles3_debug
 build linux_gles3_release linux_gles3_release
 build linux_gl_egl_debug linux_gl_egl_debug
 build linux_gl_egl_release linux_gl_egl_release
+build linux_gles3_egl_debug linux_gles3_egl_debug
+build linux_gles3_egl_release linux_gles3_egl_release
 runtest linux_gl_debug
