079d3ecd4f08cdcf229acda1d429cfd8921eef96
Andre Weissflog <floooh@gmail.com>
2024-03-02 12:04:29 +0100

    sokol_app.h emsc: add a sapp_desc.html5_emsc_set_main_loop_simulate_infinite_loop config boolean

--- sokol_app.h
+++ sokol_app.h
@@ -1717,6 +1717,7 @@ typedef struct sapp_desc {
     bool html5_bubble_key_events;       // if true, bubble up *all* key events to browser, not just key events that represent characters
     bool html5_bubble_char_events;      // if true, bubble up character events to browser
     bool html5_use_emsc_set_main_loop;  // if true, use emscripten_set_main_loop() instead of emscripten_request_animation_frame_loop()
+    bool html5_emsc_set_main_loop_simulate_infinite_loop;   // this will be passed as the simulate_infinite_loop arg to emscripten_set_main_loop()
     bool ios_keyboard_resizes_canvas;   // if true, showing the iOS keyboard shrinks the canvas
 } sapp_desc;
 
@@ -5920,7 +5921,7 @@ _SOKOL_PRIVATE void _sapp_emsc_run(const sapp_desc* desc) {
 
     // start the frame loop
     if (_sapp.desc.html5_use_emsc_set_main_loop) {
-        emscripten_set_main_loop(_sapp_emsc_frame_main_loop, 0, false);
+        emscripten_set_main_loop(_sapp_emsc_frame_main_loop, 0, _sapp.desc.html5_emsc_set_main_loop_simulate_infinite_loop);
     } else {
         emscripten_request_animation_frame_loop(_sapp_emsc_frame_animation_loop, 0);
     }
