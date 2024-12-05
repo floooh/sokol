9a6134b0009e74d1b229beb887e00474f1694807
Andre Weissflog <floooh@gmail.com>
2024-12-05 19:03:14 +0100

    switch sokol tests to dcimgui

--- tests/compile/sokol_imgui_nosokolapp.c
+++ tests/compile/sokol_imgui_nosokolapp.c
@@ -1,11 +1,10 @@
 #include "sokol_app.h"
 #include "sokol_gfx.h"
-#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #if defined(_MSC_VER )
 #pragma warning(disable:4201) // nonstandard extension used: nameless struct/union
 #pragma warning(disable:4214) // nonstandard extension used: bit field types other than int
 #endif
-#include "cimgui/cimgui.h"
+#include "cimgui.h"
 #define SOKOL_IMPL
 #define SOKOL_IMGUI_NO_SOKOL_APP
 #include "sokol_imgui.h"
