d7a77c9ead949b6ecd44e5bba71d34a7f199375d
Andre Weissflog <floooh@gmail.com>
2025-03-20 18:37:40 +0100

    sokol_app.h: fix comment

--- sokol_app.h
+++ sokol_app.h
@@ -4485,12 +4485,8 @@ static void _sapp_gl_make_current(void) {
 
 - (BOOL)performKeyEquivalent:(NSEvent*)event {
     // fixes Ctrl-Tab keydown not triggering a keyDown event
-    // NOTE: some other (function-) keys have the same behaviour, but we're
-    // not generally intercepting those because they may trigger important
-    // macOS accessibility features. If Ctrl+Fx is desired for all function keys,
-    // new options should be added to sapp_desc, similar to the HTML5 event bubbling
-    // options.
-    // NOTE 2: it seems that Ctrl-F1 cannot be intercepted the same way, but since
+    //
+    // NOTE: it seems that Ctrl-F1 cannot be intercepted the same way, but since
     // this enabled critical accessibility features that's probably a good thing.
     switch (_sapp_translate_key(event.keyCode)) {
         case SAPP_KEYCODE_TAB:
