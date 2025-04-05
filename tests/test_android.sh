a511b3e1ff07bc7df5e58ab371f1ea5079ccabb9
Andre Weissflog <floooh@gmail.com>
2025-04-05 15:15:48 +0200

    tests: remove the Android OpenSLES builds (those trigger deprecated errors now)

--- tests/test_android.sh
+++ tests/test_android.sh
@@ -4,5 +4,3 @@ source test_common.sh
 setup_android
 build android_debug android_debug
 build android_release android_release
-build android_sles_debug android_sles_debug
-build android_sles_release android_sles_release
