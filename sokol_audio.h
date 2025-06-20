eccb38d4775736a0046eb6fb326588cde231658f
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2025-06-20 15:46:28 +0800

    sokol_audio.h: remove extra semicolons

--- sokol_audio.h
+++ sokol_audio.h
@@ -1480,7 +1480,7 @@ error:
         _saudio.backend.device = 0;
     }
     return false;
-};
+}
 
 _SOKOL_PRIVATE void _saudio_alsa_backend_shutdown(void) {
     SOKOL_ASSERT(_saudio.backend.device);
@@ -1489,7 +1489,7 @@ _SOKOL_PRIVATE void _saudio_alsa_backend_shutdown(void) {
     snd_pcm_drain(_saudio.backend.device);
     snd_pcm_close(_saudio.backend.device);
     _saudio_free(_saudio.backend.buffer);
-};
+}
 
 // ██     ██  █████  ███████  █████  ██████  ██
 // ██     ██ ██   ██ ██      ██   ██ ██   ██ ██
