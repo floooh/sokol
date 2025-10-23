3a6f86d77f1ae0ce520a33cc0bf1ad2ac7c63a0d
Michaël Palomas <mpalomas@proton.me>
2025-10-23 16:47:04 +0200

    added root CMakeLists.txt: it works on Linux

---
+++ sokol-turbo.c
@@ -0,0 +1,16 @@
+#ifndef SOKOL_IMPL
+    #define SOKOL_IMPL
+#endif
+#ifndef SOKOL_NO_ENTRY
+    #define SOKOL_NO_ENTRY
+#endif
+#ifndef SOKOL_GLCORE
+    #define SOKOL_GLCORE
+#endif
+/* sokol 3D-API defines are provided by build options */
+#include "sokol_app_turbo.h"
+#include "sokol_gfx.h"
+#include "sokol_time.h"
+#include "sokol_audio.h"
+#include "sokol_log.h"
+#include "sokol_glue.h"
