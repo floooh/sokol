06f5ebe7bfe3f479954e33aa675350c950938696
Andre Weissflog <floooh@gmail.com>
2026-05-03 18:02:18 +0200

    sokol_framebuffer.h: make offscreen rendering bufferless

--- shdgen/sokol_framebuffer.glsl
+++ shdgen/sokol_framebuffer.glsl
@@ -1,15 +1,15 @@
 @vs prescale_vs
-layout(location=0) in vec2 in_pos;
-layout(location=1) in vec2 in_uv;
-
 layout(binding=0) uniform vs_params {
     vec2 uv_offset;
     vec2 uv_scale;
 };
 out vec2 uv;
+
 void main() {
-    gl_Position = vec4(in_pos*2.0-1.0, 0.5, 1.0);
-    uv = (in_uv * uv_scale) + uv_offset;
+    float x = (gl_VertexIndex & 1) != 0 ? 0.0 : 1.0;
+    float y = (gl_VertexIndex & 2) != 0 ? 1.0 : 0.0;
+    gl_Position = vec4(vec2(x, y) * 2.0 - 1.0, 0.5, 1.0);
+    uv = (vec2(x, 1.0 - y) * uv_scale) + uv_offset;
 }
 @end
 @fs rgba8_fs
